41de0ce15bf2b0340fb39d109852f6e0ee437a58
Andrew Goacher <andrewgoacher@outlook.com>
2018-10-26 22:18:59 +0100

    Basic implementation of cargo copy

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,43 @@
 #[cfg(test)]
 mod tests {
+    use build;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn can_create_paths() {
+        let src = "src";
+        let dest = "dest";
+
+        let paths = build::Paths::new(src, dest);
+        assert_eq!(src, paths.src);
+        assert_eq!(dest, paths.dest);
+    }
+}
+
+mod build {
+    use std::fs::{copy};
+
+    pub struct Paths {
+        pub src: String,
+        pub dest: String,
+    }
+
+    impl Paths {
+        pub fn new(src: &str, dest: &str) -> Paths {
+            Paths {src: String::from(src), dest: String::from(dest)}
+        }
+    }
+
+    pub fn copy_path(paths: &Paths, file: &str) -> bool {
+        let mut dest = paths.dest.clone();
+        dest.push_str(&file);
+
+        let mut src= paths.src.clone();
+        src.push_str(&file);
+
+        let result = copy(src, dest);
+        match result {
+            Ok(_) => true,
+            Err(_) => false
+        }
     }
 }
